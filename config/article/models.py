341afcf6740fe6ed3b604f7177397c67693bc36c
mohammad <ms.moraveji2007@gmail.com>
2024-01-23 21:56:24 +0330

    add article list

--- config/article/models.py
+++ config/article/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=250 , verbose_name='عنوان مقاله')
+    slug = models.SlugField(max_length=400 , db_index=True ,allow_unicode=True , verbose_name='ادرس مقاله')
+    description = models.TextField(verbose_name='توضیح مقاله')
+    image = models.ImageField(upload_to='articles')
+    is_active = models.BooleanField(default=True , verbose_name='قعال / غیر فعال')
+    date = models.DateTimeField(auto_now_add=True , editable=False , verbose_name='تاریخ ایجاد مقاله')
+    auther = models.ForeignKey(User , on_delete=models.CASCADE ,null=True, verbose_name='نویسنده')
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = ' مقاله'
+        verbose_name_plural = 'مقاله ها'
--- config/article/views.py
+++ config/article/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Article
 # Create your views here.
+
+class ArticleList(ListView):
+    model = Article
+    template_name = 'artilce/article_list.html'
+    context_object_name = 'articles'
