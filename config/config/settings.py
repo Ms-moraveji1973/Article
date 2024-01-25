08968fd8eff91f2f240540cbc6bc1aa2eac515d3
mohammad <ms.moraveji2007@gmail.com>
2024-01-25 13:05:29 +0330

    sorl-thumbnail

--- config/config/settings.py
+++ config/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'article' ,
+    'sorl.thumbnail'
 ]
 
 MIDDLEWARE = [
@@ -127,7 +128,6 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/medias/'
 
 MEDIA_ROOT = BASE_DIR / 'uploads'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
