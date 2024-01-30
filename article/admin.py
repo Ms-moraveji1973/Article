ab922c9a516c1f1849ec78207e38f2cdf2a3949a
muhammad <ms.moraveji2007@gmail.com>
2024-01-30 23:55:42 +0330

    show category in adminpanel

--- article/admin.py
+++ article/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article , Category
+
 # Register your models here.
-admin.site.register(Article)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('position','title', 'slug', 'status')
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Category , CategoryAdmin)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug' ,'status' , 'category_to_str')
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ['-status']
+
+    def category_to_str(self , obj):
+        return ". ".join([category.title for category in obj.category.all()])
+    category_to_str.short_description = 'دسته بندی'
+
+admin.site.register(Article , ArticleAdmin)
--- article/models.py
+++ article/models.py
@@ -2,14 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Category(models.Model):
+    title = models.CharField(max_length=250 , verbose_name='عنوان دسته بندی')
+    slug = models.SlugField(max_length=400 , db_index=True ,allow_unicode=True , verbose_name='ادرس دسته بندی')
+    status = models.BooleanField(default=True , verbose_name='قعال / غیر فعال')
+    position = models.IntegerField()
 
+    class Meta:
+        verbose_name = ' دسته بندی مقاله'
+        verbose_name_plural = 'دسته بندی مقاله ها'
+        ordering = ['position']
+    def __str__(self):
+        return self.title
 
 class Article(models.Model):
     title = models.CharField(max_length=250 , verbose_name='عنوان مقاله')
     slug = models.SlugField(max_length=400 , db_index=True ,allow_unicode=True , verbose_name='ادرس مقاله')
+    category = models.ManyToManyField(Category , verbose_name="دسته بندی مقاله")
     description = models.TextField(verbose_name='توضیح مقاله')
-    image = models.ImageField(upload_to='عکس مقاله')
-    is_active = models.BooleanField(default=True , verbose_name='قعال / غیر فعال')
+    image = models.ImageField(upload_to='articles')
+    status = models.BooleanField(default=True , verbose_name='قعال / غیر فعال')
     date = models.DateTimeField(auto_now_add=True , editable=False , verbose_name='تاریخ ایجاد مقاله')
     auther = models.ForeignKey(User , on_delete=models.CASCADE ,null=True, verbose_name='نویسنده')
     
