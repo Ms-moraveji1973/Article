a7b8406636a6e482ccfac213487d49da4c114cbb
muhammad <ms.moraveji2007@gmail.com>
2024-02-02 17:54:55 +0330

    FBV to CBV

--- article/urls.py
+++ article/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import ArticleList , ArticleDetail , category
+from .views import ArticleList , ArticleDetail , category , CategoryList
 urlpatterns = [
     path('' , ArticleList.as_view() , name="ArticleList" ) , 
     path('<slug:slug>' , ArticleDetail.as_view() , name="ArticleDetail" ),
-    path('category/<slug:slug>', category, name="category"),
-
+    path('category/<slug:slug>', CategoryList.as_view() , name="category"),
 ]
--- article/views.py
+++ article/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render , get_object_or_404
-from django.views.generic import ListView , DetailView
+from django.views.generic import View ,ListView , DetailView
 from .models import Article , Category
 # Create your views here.
 
@@ -20,9 +20,27 @@ class ArticleDetail(DetailView):
     model = Article
     template_name = "article/article_detail.html"
     context_object_name = 'detail'
-
+    
+# function base view for category
 def category(request , slug):
     context = {
         "category" : get_object_or_404(Category, slug=slug , status=True)
     }
     return render(request , "article/category.html" , context)
+
+# class base view for category
+class CategoryList(ListView):
+    paginate_by = 1
+    template_name = "article/category.html"
+        
+    def get_queryset(self):
+        global category
+        slug = self.kwargs.get('slug')
+        category = get_object_or_404(Category.objects.all() , slug=slug)
+        return category.articles_cat.published()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = category
+        return context
+        
