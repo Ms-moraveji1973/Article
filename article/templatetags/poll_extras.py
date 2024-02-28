78f481405db084708f71a95e418a312de31e5401
muhammad <ms.moraveji2007@gmail.com>
2024-02-29 01:34:56 +0330

    add active menu to sidebar

--- article/templatetags/poll_extras.py
+++ article/templatetags/poll_extras.py
@@ -11,3 +11,13 @@ def category_navbar():
     return {
         'category' : Category.objects.filter(status=True)
     }
+    
+    
+@register.inclusion_tag("partials/link.html")
+def link(request , link_name , content):
+    return {
+        "request" : request , 
+        "link_name" : link_name , 
+        "link" : "account:{}".format(link_name) ,
+        "content" : content,  
+    }
