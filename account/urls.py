5223ddb4c3b5e69e932fb517f901fee81341cdaa
Ms-moraveji1973 <ms.moraveji2007@gmail.com>
2024-03-08 15:35:58 +0330

    change password

--- account/urls.py
+++ account/urls.py
@@ -1,20 +1,24 @@
 from django.urls import path
 from django.contrib.auth import views
-from .views import (ArticleDelete, Home , ArticleCreate , ArticleUpdate , ArticleDelete , Profile , Login)
+from .views import (
+    ArticleDelete,
+    Home ,
+    ArticleCreate ,
+    ArticleUpdate ,
+    ArticleDelete ,
+    Profile ,
+    Login ,
+    PasswordChange,
+                    )
 
 app_name = 'account'
 urlpatterns = [
     path("login/", Login.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
     
-#    path(
-#            "password_change/", views.PasswordChangeView.as_view(), name="password_change"
-#        ),
-#        path(
-#            "password_change/done/",
-#            views.PasswordChangeDoneView.as_view(),
-#            name="password_change_done",
-#        ),
+    path("password_change/",    PasswordChange.as_view(), name="password_change"),
+    path("password_change/done/",views.PasswordChangeDoneView.as_view(),name="password_change_done",),
+    
 #       path("password_reset/", views.PasswordResetView.as_view(), name="password_reset"),
 #        path(
 #            "password_reset/done/",
--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.db.models.base import Model as Model
 from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView ,PasswordChangeView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from article.models import Article
@@ -76,3 +76,7 @@ class Login(LoginView):
             return reverse_lazy("account:home")
         else :
             return reverse_lazy("account:profile")
+        
+class PasswordChange(PasswordChangeView):
+    template_name = "registration/password_change_form.html"
+    success_url = reverse_lazy("account:password_change_done")
